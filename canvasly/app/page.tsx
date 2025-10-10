33210c43cd1e1da6cae016e969d044525da7a43b
pipes <144916002+pipesSomeAndSSam@users.noreply.github.com>
2025-10-11 01:08:11 +0800

    Initial Commit

--- canvasly/app/page.tsx
+++ canvasly/app/page.tsx
@@ -1,3 +1,5 @@
+// "use client";
+
 //import { DeployButton } from "@/components/deploy-button";
 //import { EnvVarWarning } from "@/components/env-var-warning";
 //import { AuthButton } from "@/components/auth-button";
@@ -9,8 +11,11 @@ import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 //import Link from "next/link";
 //import SearchBar from "@/components/SearchBar";
+//import { useSidebar } from "@/components/ui/sidebar";
 
 export default function Home() {
+  //const { toggleSidebar } = useSidebar();
+
   return (
     <main className="min-h-screen flex flex-col justify-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
